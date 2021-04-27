3dd427a5a11bfe4698951dc0be65da1dc4a20857
jimmycababa <jimmycababa@gmail.com>
2021-04-27 15:49:34 -0500

    added routes and controller files

---
+++ server.js
@@ -0,0 +1,23 @@
+const path = require('path');
+const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const { debugPort } = require('process');
+
+// load env variables
+dotenv.config({ path: './config/config.env'});
+
+const app = express();
+
+//body parser
+app.use(express.json());
+
+// enable cors
+app.use(cors());
+
+// routes
+app.use('/api/v1/stores', require('./routes/stores'))
+
+const PORT = process.env.PORT || 2121;
+
+app.listen(PORT, () => console.log(`Server running in ${process.env.NODE_ENV} mode on ${PORT}`));
