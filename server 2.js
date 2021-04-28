69b459f496c4b0e3a00823173acf1c7bfead8005
jimmycababa <jimmycababa@gmail.com>
2021-04-28 16:20:19 -0500

    added db.js and connection to db

--- server 2.js
+++ server 2.js
@@ -3,13 +3,9 @@ const express = require('express');
 const dotenv = require('dotenv');
 const cors = require('cors');
 const { debugPort } = require('process');
-const connectDB = require('./config/db')
 
 // load env variables
 dotenv.config({ path: './config/config.env'});
-
-// connect db
-connectDB();
 
 const app = express();
 
@@ -22,7 +18,7 @@ app.use(cors());
 // routes
 app.use('/api/v1/stores', require('./routes/stores'))
 
-//port
+//port we are on
 const PORT = process.env.PORT || 2121;
 
 app.listen(PORT, () => console.log(`Server running in ${process.env.NODE_ENV} mode on ${PORT}`));
